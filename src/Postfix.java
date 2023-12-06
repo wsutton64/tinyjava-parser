c9161ac6ac8e8970cd8cd4935ad0e69949cacc2b
wsutton64 <112420456+wsutton64@users.noreply.github.com>
2023-12-06 03:38:37 -0800

    Add comments

--- src/Postfix.java
+++ src/Postfix.java
@@ -5,21 +5,25 @@ public class Postfix {
     private String string;
 
     public Postfix() {
+        // Initialize the string and ArrayList
         string = "";
         postfixList = new ArrayList<>();
     }
 
     public void add(String str) {
+        // When called, append string with the passed string. This builds the postfix
         string += (str + " ");
     }
 
     public void submit() {
+        // When called, add the postfix string to postfixList. If the string is empty, skip it
         if (string.equals("")) return;
         postfixList.add(string);
         string = "";
     }
 
     public ArrayList<String> getList() {
+        // Return the ArrayList of postfix notation strings
         return postfixList;
     }
 }
